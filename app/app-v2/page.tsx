1fde1f4b29b2804a7eaf9d73ab03024e2d17de1b
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-06 09:37:56 +0530

    feat: register Farmer App Phase 3 component

--- app/app-v2/page.tsx
+++ app/app-v2/page.tsx
@@ -1,5 +1,10 @@
 import FarmerAppPhase2 from './phase2';
+import FarmerAppPhase3 from './phase3';
 
 export default function FarmPlugFarmerApp() {
   return <FarmerAppPhase2 />;
+}
+
+export function FarmerAppPhase3Route() {
+  return <FarmerAppPhase3 />;
 }
