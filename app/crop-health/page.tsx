bec4fa5e29e3223f87fcd9f34b6bf352a47634c6
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-06 11:39:21 +0530

    feat: add crop health route

---
+++ app/crop-health/page.tsx
@@ -0,0 +1,2 @@
+import FarmPlugOperationalScreens from '../../components/FarmPlugOperationalScreens';
+export default function CropHealthPage(){return <FarmPlugOperationalScreens mode="crop-health"/>}
