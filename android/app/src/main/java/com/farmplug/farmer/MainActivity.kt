dde23cd269da548bdfeab18c60b8b790e2c0c3d0
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-06 17:23:09 +0530

    Add Farmer Android WebView activity

---
+++ android/app/src/main/java/com/farmplug/farmer/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.farmplug.farmer
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.databaseEnabled = true
+        webView.settings.setSupportZoom(false)
+        webView.settings.userAgentString = webView.settings.userAgentString + " FarmPlugFarmer/1.0.0"
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                return false
+            }
+        }
+        webView.loadUrl("https://farmplugaisxd.vercel.app/dashboard/farmer")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+}
