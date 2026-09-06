669b3231d7cd0171f66b2556bf10f92e2736ca63
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-06 12:53:56 +0530

    Delete mobile directory

--- mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:farmplug_farmer/main.dart';
-
-void main() {
-  testWidgets('FarmPlug app can be constructed', (tester) async {
-    await tester.pumpWidget(const FarmPlugApp());
-    expect(find.text('FarmPlug AI'), findsOneWidget);
-  });
-}
