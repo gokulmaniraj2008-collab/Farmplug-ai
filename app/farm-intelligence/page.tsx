d056ce3a21b68936e9918020892c15dd3307ce52
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-06 11:39:24 +0530

    feat: add farm intelligence route

---
+++ app/farm-intelligence/page.tsx
@@ -0,0 +1,2 @@
+import FarmPlugOperationalScreens from '../../components/FarmPlugOperationalScreens';
+export default function FarmIntelligencePage(){return <FarmPlugOperationalScreens mode="farm-intelligence"/>}
