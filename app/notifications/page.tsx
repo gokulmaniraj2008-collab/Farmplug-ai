e58396f0da87aa3836e25157cf1778999f4643cb
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-06 11:39:32 +0530

    feat: add notifications route

---
+++ app/notifications/page.tsx
@@ -0,0 +1,2 @@
+import FarmPlugOperationalScreens from '../../components/FarmPlugOperationalScreens';
+export default function NotificationsPage(){return <FarmPlugOperationalScreens mode="notifications"/>}
